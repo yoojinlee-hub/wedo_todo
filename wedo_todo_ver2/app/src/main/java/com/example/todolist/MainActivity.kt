cb4ec483561f170f45b83ca3baf8635300a7a3fd
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-11-03 11:20:02 +0900

    add notification function

--- wedo_todo_ver2/app/src/main/java/com/example/todolist/MainActivity.kt
+++ wedo_todo_ver2/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Noti 객체 생성
+    private lateinit var notificationHelper: NotificationHelper
 
     private lateinit var todoAdapter : TodoAdapter
 
@@ -31,6 +35,25 @@ class MainActivity : AppCompatActivity() {
         }
         btnDeleteDoneTodo.setOnClickListener {
             todoAdapter.deleteDoneTodos()
+
+            //Noti 초기화
+            notificationHelper = NotificationHelper(this)
+
+            val titleEdit = "Wedo Todo";
+            val title: String = titleEdit.toString()
+            val messageEdit = "Congratuation\uD83D\uDC4F\uD83D\uDC4F you finished something todo! "
+            val message: String = messageEdit.toString()
+
+                //알림 호출
+            showNotification(title, message)
         }
+    }
+    //알림 호출
+    private fun showNotification(title: String, message: String){
+
+        val nb: NotificationCompat.Builder =
+            notificationHelper.getChannelNotification(title, message)
+
+        notificationHelper.getManager().notify(1, nb.build())
     }
 }
